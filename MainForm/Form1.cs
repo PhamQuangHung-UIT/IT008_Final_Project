afe53d085969ae1e3b77460805fc0989b7a57839
PhuDangSaP <21522464@gm.uit.edu.vn>
2022-11-27 16:21:42 +0700

    mainform

---
+++ MainForm/Form1.cs
@@ -0,0 +1,45 @@
+﻿namespace MainForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tvHD.Nodes.Add(root1);           
+            tvHD.Nodes.Add(root2);
+            root1.Text = "Hóa đơn đang xử lý";
+            root2.Text = "Hóa đơn đã kết thúc";
+            tvHD.NodeMouseDoubleClick += TvHD_NodeMouseDoubleClick;
+        }
+
+        private void TvHD_NodeMouseDoubleClick(object? sender, TreeNodeMouseClickEventArgs e)
+        {
+            FHoadon x = new FHoadon();
+            x.Show();
+        }
+
+       
+
+        TreeNode root1=new TreeNode();  
+        TreeNode root2 = new TreeNode();
+
+
+        
+       
+
+        private void btnCreateHD_Click(object sender, EventArgs e)
+        {
+            TreeNode nodex= new TreeNode();
+            nodex.Text = "Hóa đơn 1";
+            root1.Nodes.Add(nodex);           
+            Form3 a=new Form3();
+            a.Show();
+            
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
